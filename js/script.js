9d027cc4ab909054058c9fab744928e76efec3a2
Dan Vuong <vtdanh0701@gmail.com>
2019-06-05 05:04:32 -0700

    slideshow function & layout

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 var hide = document.getElementById("inputcontainer");
 var resetButton = document.getElementById("reset");
-function test(){
-    console.log("a");
-    hide.style.display = "none";
-}
+var imgDisplay = document.getElementById("img");
+var images = ["1","2","3","4","5","6"];
+var i=0;
+var on = true;
+var t;
+//imgDisplay.src = "img/1.jpg";
 
+function play(){
+    console.log(on);
+    hide.style.display = "none";
+    on = true;
+}
+function clickButton(){
+    play();
+    slideShow();
+}
 function reset(){
     hide.style.display = "block";
-    
+    on = false;
+    console.log(on);
+    clearTimeout(t);
 }
 
 //get input
@@ -20,12 +33,40 @@ function reset(){
 // display img
 // reset
 
-fetch('http://www.reddit.com/search.json?q=cats+nsfw:no')
-    .then(function(data){
-        return data.json();
-    })
-    .then(function(json){
-        var img = json;
-        var url = img.data.children[0].data.url
-        console.log(url)
-    })
+// fetch('http://www.reddit.com/search.json?q=cats+nsfw:no')
+//     .then(function(data){
+//         return data.json();
+//     })
+//     .then(function(json){
+//         var img = json;
+//         var url = img.data.children[0].data.url
+//         console.log(url)
+//     })
+
+
+function slideShow(){
+    imgDisplay.src = "img/"+ images[i] +".jpg";
+    if((i < images.length - 1) && on === true){
+        i++;
+        console.log(on);
+    }
+    else if(on === true && i === images.length - 1){
+        i = 0;
+        console.log(on);
+    }
+    t = setTimeout(slideShow,2000);
+}
+
+function pause(){
+    if(on === true){
+        on = false;
+        document.getElementById("clickplay").textContent = "Play";
+        console.log(on);
+        clearTimeout(t);
+    }
+    else if(on !== true){
+        on = true;
+        slideShow();
+        document.getElementById("clickplay").textContent = "Pause";
+    }
+ }
